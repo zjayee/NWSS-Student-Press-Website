a774c120aa81eaa954819572c8ec555c1e7c7b7c
ZJayee <jayee.zheng@gmail.com>
2021-10-11 14:41:38 -0700

    Add middle bar

---
+++ src/components/MiddleBar.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import instagram from '../images/instagram.svg'
+import{link, text, container, img} from './MiddleBar.module.css'
+
+const MiddleBar = () =>{
+
+    return(
+       <div className={container}>
+           <p className= {text}>Follow Us On Social Media</p>
+            <a href="https://www.instagram.com/nwssstudentpress/"className={link}><img src= {instagram} className={img}/> @nwssstudentpress</a>
+       </div>
+    )
+
+}
+
+export default MiddleBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,19 +7,21 @@ import NextButton from '../components/NextButton'
 import BackButton from '../components/Backbutton'
 import ArchiveHeader from '../components/ArchiveHeader'
 import TopNav from '../components/TopNav'
+import MiddleBar from '../components/MiddleBar'
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
       <TopNav/>
+      <MiddleBar/>
        <BackButton/><NextButton/>
       <ArchiveCard month="June" year="2021" description="description description description" 
       image="https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/4e/81/5a/4e815afd-9be1-2c3c-9f79-db523e722d49/190296479854.jpg/400x400cc.jpg"
       url = "/about"
       />
       <ArchiveHeader/>
-     
+      
     </main>
   )
 }
