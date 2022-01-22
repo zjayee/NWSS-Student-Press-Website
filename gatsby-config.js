b335b16014cbc1e7c66bb505d8db93289e49b5f2
ZJayee <jayee.zheng@gmail.com>
2022-01-21 23:31:03 -0800

    contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "NWSS Student Press",
   },
-  plugins: [],
+  plugins: [
+
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+
+  ],
 };
---
+++ src/pages/ArticleViewerPage.js
@@ -0,0 +1,16 @@
+
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+
+const ArticleViewerPage = () => {
+  return (
+    <main>
+      <title></title>
+      
+    </main>
+  )
+}
+
+
+export default ArticleViewerPage
